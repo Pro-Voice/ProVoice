7e905eb5051128e91e246c9192fa9a5bf25e1817
pinsclub <44050593+Pro-Voice@users.noreply.github.com>
2021-08-08 17:19:17 +0900

    Install @expo/vector-icons

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import {
+  View, ScrollView, Text, StyleSheet,
+} from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
@@ -13,11 +15,13 @@ export default function MemoDetailScreen() {
         <Text style={styles.memoDate}>2021年8月8日 10:00</Text>
       </View>
       <ScrollView style={styles.memoBody}>
-        <Text style={styles.momoText}>
-          買い物リスト書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
+        <Text style={styles.memoText}>
+          買い物リスト
+          買い物リスト書体やレイアウトなどを確認するために用います。
+          本文用なので使い方を間違えると不自然に見えることもありますので要注意。
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }} name="edit-2" />
     </View>
   );
 }
